c352c9068737bc0009b384eca3b4c342b86878dc
LAPTOP-6HJUC4E9\justi <justin555@live.nl>
2017-02-21 16:46:02 +0100

    Final version

--- Js/Bal.js
+++ Js/Bal.js
@@ -1,13 +1,18 @@
 var turns = 0;
+var score = document.getElementById('score');
+var balImg = document.getElementById("image");
+var balName = document.getElementById("ball");
+var imgCard = document.getElementById('card');
+
 if (localStorage.score) {
-	document.getElementById('score').innerHTML =  localStorage.score;
+	score.innerHTML =  localStorage.score;
 }
 	else {
 	localStorage.score = 0;
-	document.getElementById('score').innerHTML =  localStorage.score;
+	score.innerHTML =  localStorage.score;
 }
-function grabBal() {
 
+function grabBal() {
 	if (turns === 3) {
 		alert("Out of turns");
 		return false;
@@ -15,19 +20,48 @@ function grabBal() {
 		turns++;
 		var balChane = Math.floor(Math.random() * 100) + 1
 		if (balChane <= 5 && balChane >= 0 ) {
-			document.getElementById("image").innerHTML="<img src='img/Green.png' />";
-			document.getElementById("ball").innerHTML = "groen";
+			balImg.innerHTML="<img src='img/Green.png' />";
+			balName.innerHTML = "groen";
 			localStorage.score = Number(localStorage.score) + 1000;
 		} else if (balChane <= 80 && balChane >= 6) {
-			document.getElementById("image").innerHTML="<img src='img/Blue.png' />";
-			document.getElementById("ball").innerHTML = "blauw";
+			balImg.innerHTML="<img src='img/Blue.png' />";
+			balName.innerHTML = "blauw";
+			maingen();
 			localStorage.score = Number(localStorage.score) + 150;
 		} 	else {
-			document.getElementById("image").innerHTML="<img src='img/Rood.png' />";
-			document.getElementById("ball").innerHTML = "rood";
+			balImg.innerHTML="<img src='img/Rood.png' />";
+			balName.innerHTML = "rood";
 			localStorage.score = Number(localStorage.score) - localStorage.score
 			turns = 3;
 		}
 	}
-	document.getElementById('score').innerHTML =  localStorage.score;
+	score.innerHTML =  localStorage.score;
+}
+
+function getRandomImage(imgAr, path) {
+    path = path || 'img/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    imgCard.innerHTML = imgStr;
+}
+
+function balCard() {
+	var random_images_array = ["LingoCard1_balOut.png", "LingoCard1.png",
+	 "LingoCard2_balOut.png", "LingoCard2.png"];
+	 getRandomImage(random_images_array);
+}
+
+function generate_rand() {
+    return Math.floor((Math.random() * 75));
+}
+
+function maingen() {
+    var balNumber = generate_rand() + 1;
+    balGone(balNumber);
+}
+
+function balGone(balNumber) {
+	document.getElementById('tesst').innerHTML = "Bal number is " + balNumber;
+	var gifChoose = document.getElementById('gifie');
 }
